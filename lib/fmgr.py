e2878ec986ca47fc72e826fd357d8ace92732574
Lucas Messenger <lucas.c.messenger@gmail.com>
2016-08-28 21:04:10 -0700

    first commit of fmgr and hdiutil libraries

---
+++ lib/fmgr.py
@@ -0,0 +1,38 @@
+import os
+
+
+def checkdirexists(dir):
+
+    # check if the path exists
+    if os.path.exists(dir) == True:
+
+        # if true, check if the path is a directory
+        if os.path.isdir(dir) == True:
+            return True
+        else:
+            # path is not a directory, provide an error
+            print("ERROR: " + dir + " already exists and is not a directory.")
+            return True # need to figure out a better returned value
+    else:
+        return False
+
+# make directory
+def makedir(dir):
+
+    # if the dir doesn't exist, built it
+    if checkdirexists(dir) == False:
+        os.mkdir(dir)
+
+# remove directory
+def removedir(dir):
+
+    # if the directory exists, and it is a directory, remove it
+    if checkdirexists(dir) == True:
+        os.rmdir(dir)
+
+# remove file
+def removefile(file):
+
+    # remove file
+    if os.path.exists(file) == True:
+        os.remove(file)
---
+++ lib/hdiutil.py
@@ -0,0 +1,25 @@
+import os
+from fmgr import makedir, removedir, checkdirexists
+
+
+def diskmounter(fpath, mountpoint, fullname):
+
+    # let's build the mountpoint
+    makedir(mountpoint)
+
+    # the reason for -mountpoint allows us to force the directory to mount to
+    # a specific location we own
+    os.system("hdiutil attach -mountpoint " +
+              mountpoint + " " + fpath + " -nobrowse -quiet")
+    print(fullname + " mounted.")
+
+
+def diskunmounter(mountpoint, fullname):
+
+    if checkdirexists(mountpoint) == True:
+
+        # it's a good thing we know the mountpoint
+        os.system("hdiutil detach " + mountpoint + " -quiet")
+        print(fullname + " unmounted.")
+    else:
+        print(mountpoint + " not found.")
